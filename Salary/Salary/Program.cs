97a5ef174d96fbd320ce608c9087fa861aacc10a
ndayishimiyedamas <58144395+ndayishimiyedamas@users.noreply.github.com>
2019-11-25 06:34:25 +0900

    Add files via upload

---
+++ Salary/Salary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this system will calculate salary of employee!");
+            Console.ReadLine();
+            var Employee1=new SalaryCalculator( 1, "james", "section");
+            Console.ReadLine();
+            Console.WriteLine("your net salary: " + SalaryCalculator.CalculateSalary(500000));
+            Console.WriteLine("your Hourly salary:"+SalaryCalculator.CalculateHourly(12, 34));
+            Console.WriteLine("your Hourly salary:"+ SalaryCalculator.CalculateDaily(12));
+        }
+    }
+}
